3291c11bfe1166f883007704d05023628efd6859
ruoxuan0318 <wongruoxuan0318@gmail.com>
2026-04-30 09:12:22 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,13 +1,1 @@
-<footer style="background:#f8f8f8; padding:20px; text-align:center; margin-top:50px;">
 
-    <p>🌸 Online Florist</p>
-
-    <p>© 2026 All rights reserved.</p>
-
-    <p>
-        <a href="homepage.php">Home</a> |
-        <a href="category.php">Category</a> |
-        <a href="profile.php">Profile</a>
-    </p>
-
-</footer>
