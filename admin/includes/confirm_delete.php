7195ab8117acfdd0da3368a5cf7a63623269c76c
huishannn <LIM.HUI.SHAN@student.mmu.edu.my>
2026-05-08 23:43:50 +0800

    Create confirm_delete.php

---
+++ admin/includes/confirm_delete.php
@@ -0,0 +1,19 @@
+<div class="modal" id="confirmDeleteModal">
+    <div class="modal-card" style="max-width:460px;">
+        <div class="modal-header">
+            <h3 style="margin:0;">Confirm Delete</h3>
+            <button type="button" class="icon-btn" data-close-modal><i class='bx bx-x'></i></button>
+        </div>
+
+        <div class="modal-body">
+            <p>Are you sure you want to delete this item?</p>
+            <p style="color:#8c8c8c">This action cannot be undone.</p>
+
+            <div class="modal-actions">
+                <button type="button" class="btn btn-ghost" data-close-modal>Cancel</button>
+                <button type="button" class="btn btn-danger" id="confirmDeleteGo">Delete</button>
+            </div>
+            
+        </div>
+    </div>
+</div>
